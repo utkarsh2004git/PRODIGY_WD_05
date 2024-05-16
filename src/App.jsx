a814887615cd7bc4f820bf47dc1d187890a6af93
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-05-17 01:21:37 +0530

    added footer and changed css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import TimeAndLocation from './components/TimeAndLocation';
 import TempAndDetails from './components/TempAndDetails';
 import Forecast from './components/Forecast';
 import getFormattedWeatherData from './services/weatherServices';
+import Footer from './services/weatherServices';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -20,10 +21,10 @@ const App = () => {
 
   const getWeather = async () => {
 
-    const message=query.q?query.q :"current location";
-    toast.info(`Fetching weather data for ${message.charAt(0).toUpperCase()+message.slice(1)}`)
+    const message = query.q ? query.q : "current location";
+    toast.info(`Fetching weather data for ${message.charAt(0).toUpperCase() + message.slice(1)}`)
 
-    const data = await getFormattedWeatherData({...query,units})
+    const data = await getFormattedWeatherData({ ...query, units })
       .then(data => { setWeather(data) })
   };
 
@@ -32,29 +33,46 @@ const App = () => {
   }, [query, units])
 
 
-  const formatBackground=()=>{
-      return "from-cyan-600 to-blue-700"
+  const formatBackground = () => {
+    return "from-cyan-600 to-blue-700"
   }
 
 
   return (
-    <div className={`mx-auto max-w-screen-lg  sm:mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400  text-white ${formatBackground()}`}>
-      <TopButtons  setQuery={setQuery}/>
-      <Inputs setQuery={setQuery} setUnits={setUnits} units={units} />
-      {
-        weather && (
-          <>
-            <TimeAndLocation weather={weather}/>
-            <TempAndDetails weather={weather}  units={units}/>
-            <Forecast title='3 hours step forecast' data={weather.hourly} />
-            <Forecast title='Daily forecast' data={weather.daily} />
+    <>
+      <div className={`mx-auto w-fit  sm:max-w-screen-lg  sm:mt-4 py-5 px-10 sm:px-32 bg-gradient-to-br   text-white ${formatBackground()}`}>
+        <TopButtons setQuery={setQuery} />
+        <Inputs setQuery={setQuery} setUnits={setUnits} units={units} />
+        {
+          weather && (
+            <>
+              <TimeAndLocation weather={weather} />
+              <TempAndDetails weather={weather} units={units} />
+              <Forecast title='3 hours step forecast' data={weather.hourly} />
+              <Forecast title='Daily forecast' data={weather.daily} />
 
-          </>
-        )
-      }
-      <ToastContainer autoClose={1000} hideProgressBar={true} theme='colored' />
+            </>
+          )
+        }
 
+        <ToastContainer autoClose={1000} hideProgressBar={true} theme='colored' />
+
+      </div>
+
+      <div className=''>
+  <div className={`w-[33rem] sm:w-auto sm:max-w-screen-lg sm:mx-auto m-0 sm:mt-4 py-5 sm:px-32 text-white bg-gray-800`}>
+    <div className='flex flex-col sm:flex-row justify-center items-center gap-2 text-center sm:text-left'>
+      &copy; All Rights reserved 2024. This app is developed by <span className='font-bold text-white'>Utkarsh Shahare</span>
     </div>
+    <div className='flex  sm:flex-row justify-center items-center gap-2 text-center sm:text-left mt-2'>
+      API: <a className='text-blue-800 font-bold underline' target='_blank' href="https://openweathermap.org/api">OpenWeatherMap</a>
+    </div>
+  </div>
+</div>
+
+
+
+    </>
   )
 }
 
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -12,7 +12,7 @@ const TopButtons = ({setQuery}) => {
     ]
 
     return (
-        <div className='flex items-center justify-around my-6'>
+        <div className='flex items-center justify-around my-6 '>
             {
                 cities.map((city ) => (
                         
